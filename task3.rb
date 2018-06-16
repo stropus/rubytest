d8fa8124b30f5ca896a50e1b549d6115d8bdd028
stropus <stropus@i.ua>
2018-06-16 19:09:16 +0300

    Update task3.rb

--- task3.rb
+++ task3.rb
@@ -1,44 +1,88 @@
 t1  = ARGV[0].to_i
 t2  = ARGV[1].to_i
 
+def re (d1, tt)
+    d2 = d1 % 10 # последняя цифра
 
-#Прекращаем выполнение программы, если 1-й аргумент меньше 1
-abort "Аргумент t1: '#{t1}' должен быть больше или равен 1" if t1 < 1
+    if d1 >=11 and d1 <= 14 then
+        if tt == 1
+            rezu = "часов"
+        elsif tt == 2
+            rezu = "минут"
+        else
+            rezu ="секунд"
+        end
+        return rezu
+    end
 
-#Прекращаем выполнение программы, если 1-й аргумент больше 100000
-abort "Аргумент t2: '#{t2}' должен быть меньше или равен 100000" if t2 > 100000
+    if d1 >=2 and d1 <= 4 then
+        if tt == 1
+            rezu = "часа"
+        elsif tt == 2
+            rezu = "минуты"
+        else
+            rezu ="секунды"
+        end
+        return rezu
+    end
 
-t0 = t1 + t2
+    if d2 >= 2 and d2 <= 4 then
+        if tt == 1
+            rezu = "часа"
+        elsif tt == 2
+            rezu = "минуты"
+        else
+            rezu ="секунды"
+        end
+        return rezu
+    end
 
-secund = 0
-minut = 0
-hour = 0
+    if d1 == 1 or d2 == 1 then
+        if tt == 1
+            rezu = "час"
+        elsif tt == 2
+            rezu = "минута"
+        else
+            rezu ="секунда"
+        end
+        return rezu
+    end
 
-#достаем секунды
-secund = t0 % 60
-#убиваем секунды
-t0 /= 60
+    if tt == 1
+        rezu = "часов"
+    elsif tt == 2
+        rezu = "минут"
+    else
+        rezu = "секунд"
+    end
+    return rezu
 
-#достаем минуты
-minut = t0 % 60
-#убиваем минуты
-t0 /= 60
-
-hour = t0
-
-#Готовим вывод на экран
-rez = ""
-
-if hour != 0
-    rez = "#{hour} час "
 end
 
-if minut != 0
-    rez = rez + "#{minut} минут "
-end
 
-if secund != 0
-    rez = rez + "#{secund} секунд"
-end
+if t1 >= 1 and t2 <= 100000 then
+    t0 = t1 + t2
+    secund = 0
+    minut = 0
+    hour = 0
+    secund = t0 % 60   #достаем секунды
+    t0 /= 60           #убиваем секунды
+    minut = t0 % 60    #достаем минуты
+    t0 /= 60           #убиваем минуты
+    hour = t0          # получили часы
+    rez = ""           #Готовим вывод на экран
 
-puts rez
+    if hour != 0 then
+        rez = "#{hour} " + re(hour,1) + " "
+    end
+
+    if minut != 0 then
+        rez = rez + "#{minut} " + re(minut,2) + " "
+    end
+
+    if secund != 0 then
+       rez = rez + "#{secund} " + re(secund,3)
+    end
+
+    puts rez #Вывод результатов
+end
